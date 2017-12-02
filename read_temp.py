87cd78b74b64ec5b22fc7defeca979e5fe1836e8
theschitz <alex@theschitz.se>
2017-12-02 10:51:05 +0100

    Fixed white space and added a TODO.
    TODO is mostly to give you a sense of which direction i think you should be moving in.

--- read_temp.py
+++ read_temp.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python
-
 import Adafruit_DHT
-
-#Setup vars
+#Setup global variables
 sensor = Adafruit_DHT.DHT22
 PIN17 = 17
 PIN27 = 27
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, PIN17)
-
-
-# Here goes changes later on
-# will change print and make it push data to DB
-
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
 
-# testing a repeat with other pin
-
-
-
+# Testing a repeat with other pin
 humidity, temperature = Adafruit_DHT.read_retry(sensor, PIN27)
-
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
+
+
+#TODO: Get some kind of a loop going.    
+#TODO: Refactor current sensor calls to a function.
+#TODO: Handle Prints, Remember they are still useful for debugging.
+#TODO: Push data to DB.
+  
